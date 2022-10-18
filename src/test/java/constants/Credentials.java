4d7e66928998733aedda106dd5286a46d4fbde41
OlgaBobryk <oljunja-86@mail.ru>
2022-10-18 13:37:33 +0300

    Credentials.java add

---
+++ src/test/java/constants/Credentials.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class Credentials {
+    public static final String USERNAME_FOR_LOGIN = "cvbnnbvc";
+    public static final String PASSWORD_FOR_LOGIN = "111111";
+    public static final String REMOTE_URL="https://OlgaBobryk:80c3a436-5d99-4e37-b9ef-96e69790bb74@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+
+
+}
